aa11b95ed1065fa29515c0c403ce6eb3f860b634
wk <972509350@qq.com>
2021-03-01 00:15:48 +0800

    mysql全文检索

--- src/main/java/com/wk/controller/SearchController.java
+++ src/main/java/com/wk/controller/SearchController.java
@@ -7,11 +7,9 @@ import com.wk.vo.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
